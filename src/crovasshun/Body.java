72d5609266ffac57aebc85cfc08dd7d50377f25e
Angle <Ribbonwing@gmail.com>
2017-08-22 15:23:14 -0600

    Added beginning of code for units.

---
+++ src/crovasshun/Body.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crovasshun;
+
+import display.ASCIISprite;
+import java.awt.Point;
+
+/**
+ *
+ * @author angle
+ */
+public class Body {
+    public Point position;
+    public ASCIISprite sprite;
+    
+}
---
+++ src/display/ASCIISprite.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package display;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author angle
+ */
+public class ASCIISprite {
+    
+    public final Color color;
+    public final char character;
+    
+    public ASCIISprite(Color color, char character) {
+        this.color = color;
+        this.character = character;
+    }
+    
+}
