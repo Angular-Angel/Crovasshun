7e6faf98ad3f77bcb86c7352eff48c1d49aa8ad5
Angle <Ribbonwing@gmail.com>
2017-10-06 11:16:21 -0600

    Added base code for BodyActions and implementation for MoveAction

---
+++ src/crovasshun/MoveAction.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crovasshun;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author angle
+ */
+public class MoveAction implements BodyAction {
+    
+    public ArrayList<Point> points;
+    
+    public MoveAction() {
+        points = new ArrayList<>();
+    }
+    
+    public void addPoint(Point point) {
+        points.add(point);
+    }
+    
+    public void removePoint() {
+        if (points.size() > 0)
+            points.remove(points.size() - 1);
+    }
+
+    @Override
+    public void perform(Body body) {
+        
+    }
+
+    @Override
+    public boolean isValid() {
+        
+        return true;
+    }
+    
+}
